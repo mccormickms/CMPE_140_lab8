9c5165b4e60d738f84519d778095cb4e010ca7b7
Matthew McCormick <mccormick.ms@gmail.com>
2018-04-16 18:10:34 -0700

    Add files via upload

---
+++ controlunit.v
@@ -0,0 +1,9 @@
+module controlunit
+(input zero, [5:0] opcode, funct, 
+	output pc_src, jump, reg_dst, we_reg, alu_src, we_dm, dm2reg, PCtoReg,
+		   [6:0] alu_ctrl);
+    wire [1:0] alu_op;
+    assign pc_src = branch & zero;
+    maindec md (opcode, branch, jump, reg_dst, we_reg, alu_src, we_dm, dm2reg, PCtoReg, alu_op);
+    auxdec  ad (alu_op, funct, alu_ctrl);
+endmodule
---
+++ mips.v
@@ -0,0 +1,11 @@
+module mips
+(input clk, rst, [4:0] ra3, [31:0] instr, rd_dm, 
+ output we_dm, [31:0] pc_current, alu_out, wd_dm, rd3);
+    wire       pc_src, jump, reg_dst, we_reg, alu_src, dm2reg, PCtoReg;
+    wire [6:0] alu_ctrl;
+    datapath    dp (clk, rst, pc_src, jump, reg_dst, we_reg, alu_src, 
+    				dm2reg, PCtoReg, alu_ctrl, ra3, instr, rd_dm, zero, 
+    				pc_current, alu_out, wd_dm, rd3);
+    controlunit cu (zero, instr[31:26], instr[5:0], pc_src, jump, reg_dst, 
+    				we_reg, alu_src, we_dm, dm2reg, PCtoReg, alu_ctrl);
+endmodule
